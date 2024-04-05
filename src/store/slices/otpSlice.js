f99513146b8552f9110476f2c3ba297fdead41c0
grpoudel <mabisha.dahal@braincast.ai>
2024-04-05 06:27:28 +0545

    API URL updated

--- src/store/slices/otpSlice.js
+++ src/store/slices/otpSlice.js
@@ -1,6 +1,6 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 import axios from "axios";
-const baseURL = "http://localhost:4000";
+const baseURL = process.env.API_URL || "http://localhost:4000";
 export const validateOtp = createAsyncThunk("otp/validate", async (otp) => {
   try {
     const response = await axios.post(baseURL + "/api/validate_otp", otp);
